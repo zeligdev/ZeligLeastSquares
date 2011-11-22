5c68e46d2b524b3320c548d04d6e7809b0c01554
Matt <mattisbusiness@gmail.com>
2011-11-22 13:24:16 -0500

    Commiting changes for ThxGiving

--- R/describe.twosls.R
+++ R/describe.twosls.R
@@ -1,10 +1,24 @@
-#' Describe the twosls Zelig Model
+#' Describe the threesls Zelig Model
 #' @param ... ignored parameters
 #' @return a list specifying author, title, etc. information
 #' @export
-describe.twosls <- function(...) {
-  list(
-       authors = "",
-       text = ""
-       )
+describe.twosls<-function(...){
+category <- "continuous"
+description  <- "Two Stage Least Squares"
+authors <- c("Ferdinand Alimadhi","Ying Lu", "Elena Villalon")
+year <- 2007
+package <-list(	name 	="systemfit",
+		version	="0.8"
+		)
+parameters<-list(mu="mu", inst="inst")
+parameters$mu<-list(equations=c(2,Inf),
+			tagsAllowed=TRUE,
+			depVar=TRUE,
+			expVar=TRUE)
+			
+parameters$inst<-list(equations=c(1,1),
+			tagsAllowed=FALSE,
+			depVar=FALSE,
+			expVar=TRUE)
+list(category = category, authors = authors, year = year,description=description,package=package,parameters=parameters)
 }
--- R/zelig2twosls.R
+++ R/zelig2twosls.R
@@ -6,9 +6,33 @@
 #' @return a list specifying '.function'
 #' @export
 zelig2twosls <- function (formula, ..., data) {
+
+  # Helper function to perform set-difference
+  "%w/o%" <- function(x, y)
+    x[!x %in% y]
+
+  formula.eqs <- names(getDependentVariables(formula))
+  instrument.eqs <- names(formula) %w/o% formula.eqs
+
+  # 
+  inst <- if (length(instrument.eqs) == 1)
+    formula[[instrument.eqs]]
+
+  else if (length(instrument.eqs) > 1)
+    formula[instrument.eqs]
+
+  else
+    stop("...")
+
+  class(formula) <- c("multiple", "list")
+
+  # Return
   list(
-       .function = "",
-       formula = formula,
-       data = data
+       .function = "callsystemfit",
+       formula = formula[formula.eqs],
+       method  = "2SLS",
+       inst    = inst,
+       data = data,
+       ...
        )
 }
